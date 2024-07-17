cd52cfbad30c5ddf51823c6ad89ff31bea0ed15d
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2024-07-17 18:57:16 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Running java programs");
+        System.out.println(new Date());
+        System.out.println("Hello");
+        System.out.println("Singh");
+        System.out.println("Hello buddy");
+    }
+}
